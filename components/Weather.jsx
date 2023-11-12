710408796144b3a78bbbb5021030873f71939317
Steve Wichelecki <wichelecki_steve@shure.com>
2023-11-12 09:35:22 -0600

    Removed geolocation

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -13,15 +13,13 @@ const ICON_MAP = new Map();
 
 const Weather = () => {
   const [temperature, setTemperature] = useState(0);
-  const [location, setLocation] = useState('');
   const [weatherCode, setWeatherCode] = useState(0);
   const [todaysHigh, setTodaysHigh] = useState(0);
   const [todaysLow, setTodaysLow] = useState(0);
 
-  const getWeather = async (latitude, longitude, timezone) => {
-    const openMeteoApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=${timezone}&forecast_days=1`;
-    /*   const openMeteoApiUrl =
-      'https://api.open-meteo.com/v1/forecast?latitude=41.9231&longitude=-87.7093&current=temperature_2m,weather_code&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FChicago&forecast_days=1'; */
+  const getWeather = async () => {
+    const openMeteoApiUrl =
+      'https://api.open-meteo.com/v1/forecast?latitude=41.9231&longitude=-87.7093&current=temperature_2m,weather_code&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FChicago&forecast_days=1';
     const response = await fetch(openMeteoApiUrl);
 
     if (response?.status >= 200 && response?.status <= 299) {
@@ -31,7 +29,6 @@ const Weather = () => {
       const { temperature_2m_max, temperature_2m_min } = daily;
 
       setTemperature(Math.round(temperature_2m));
-      setLocation(timezone.split('/')[1]);
       setWeatherCode(weather_code);
       setTodaysHigh(Math.round(temperature_2m_max));
       setTodaysLow(Math.round(temperature_2m_min));
@@ -44,33 +41,7 @@ const Weather = () => {
   };
 
   useEffect(() => {
-    const positionSuccess = ({ coords }) => {
-      const { latitude, longitude } = coords;
-
-      getWeather(
-        latitude,
-        longitude,
-        Intl.DateTimeFormat().resolvedOptions().timeZone
-      );
-    };
-
-    const positionError = () => {
-      console.log('Error getting location');
-    };
-
-    const options = {
-      enableHighAccuracy: false,
-      timeout: 5000,
-      maximumAge: 900000,
-    };
-
-    navigator.geolocation.getCurrentPosition(
-      positionSuccess,
-      positionError,
-      options
-    );
-
-    //getWeather(false, false, Intl.DateTimeFormat().resolvedOptions().timeZone);
+    getWeather();
   }, []);
 
   useEffect(() => {
@@ -106,7 +77,7 @@ const Weather = () => {
             <span>&deg;</span>
           </p>
         </div>
-        <p>{location}</p>
+        <p>Chicago</p>
       </div>
       <div className='weather__high-low-wrapper'>
         <p>
