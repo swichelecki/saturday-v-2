2c2f4fefef8ab0fb6356fa2cf5a6806e20806596
Steve Wichelecki <wichelecki_steve@shure.com>
2023-12-23 13:15:57 -0600

    Added database connection caching

--- config/db.js
+++ config/db.js
@@ -1,8 +1,25 @@
 import mongoose from 'mongoose';
 
+const MONGO_URI = process.env.MONGO_URI;
+
+let cached = mongoose || { conn: null, promise: null };
+
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+  if (cached.conn) return cached.conn;
+
+  if (!MONGO_URI) throw new Error('MONGO_URI is missing');
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGO_URI, {
+      dbName: 'saturday_db',
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+  console.log('MongoDB Connected');
+
+  return cached.conn;
 };
 
 export default connectDB;
