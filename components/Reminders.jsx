b6853e89ecf25c35bf548b1fc94841a4839a5c6d
Steve Wichelecki <wichelecki_steve@shure.com>
2024-03-24 09:22:09 -0500

    Added Reminder reset button functionality

--- components/Reminders.jsx
+++ components/Reminders.jsx
@@ -1,18 +1,68 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { useAppContext } from 'context';
 import { RemindersItem } from 'components';
+import { getReminder, updateReminder } from '../services';
 import { handleHiddenHeight } from 'utilities';
 import moment from 'moment-timezone';
 
 const Reminders = ({ reminders }) => {
+  const { setShowToast, setServerError } = useAppContext();
+
   const remindersWrapperRef = useRef(null);
 
   const [remindersItems, setReminders] = useState(reminders ?? []);
+  const [reminderToUpdate, setReminderToUpdate] = useState({});
   const [showReminders, setShowReminders] = useState(true);
   const [isAwaitingResetResponse, setIsAwaitingResetResponse] = useState(false);
 
+  // set next reminder date
+  useEffect(() => {
+    if (isAwaitingResetResponse) {
+      let copyOfReminderToUpdate = { ...reminderToUpdate };
+
+      const today = new Date();
+      const startingDate = copyOfReminderToUpdate?.reminderDate;
+      const interval = copyOfReminderToUpdate?.recurrenceInterval;
+      const date = new Date(startingDate);
+      date.setMonth(today.getMonth() + interval);
+      const nextDate = date.toISOString();
+
+      copyOfReminderToUpdate = {
+        ...copyOfReminderToUpdate,
+        displayReminder: false,
+        reminderDate: nextDate,
+      };
+
+      updateReminder(copyOfReminderToUpdate).then((res) => {
+        if (res.status === 200) {
+          setReminders(
+            remindersItems.filter((item) => item._id !== reminderToUpdate._id)
+          );
+        }
+
+        if (res.status !== 200) {
+          setServerError(res.status);
+          setShowToast(true);
+        }
+
+        setIsAwaitingResetResponse(false);
+      });
+    }
+  }, [reminderToUpdate]);
+
+  // get reminder to update
   const handleResetReminder = (id) => {
     setIsAwaitingResetResponse(true);
-    // TODO: add reset logic
+    getReminder(id).then((res) => {
+      if (res.status === 200) {
+        setReminderToUpdate(res.item);
+      }
+
+      if (res.status !== 200) {
+        setServerError(res.status);
+        setShowToast(true);
+      }
+    });
   };
 
   return (
